91b6beaea63c67056783250c4815d53f78125118
Melis-12210167 <Melismel.mm@gmail.com>
2022-10-10 14:49:13 +0700

    edit by melis

---
+++ app/Controllers/KategoriKontroller.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Controllers\BaseController;
+
+class KategoriKontroller extends BaseController
+{
+    public function index()
+    {
+        //
+    }
+}
